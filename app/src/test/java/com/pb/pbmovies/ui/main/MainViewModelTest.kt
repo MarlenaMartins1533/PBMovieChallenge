3514772c334cf05de3b48b5145c720c7b198f5b8
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2025-01-20 21:13:18 +0100

    Update and fix ViewModelTest.

--- app/src/test/java/com/pb/pbmovies/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/pb/pbmovies/ui/main/MainViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.pb.pbmovies.ui.main
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.pb.pbmovies.Resources
 import com.pb.pbmovies.domain.usecase.GetMovieUseCase
 import com.pb.pbmovies.ui.composable.mapper.MovieToItemMovieMapper
@@ -9,31 +8,25 @@ import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainViewModelTest {
 
-    @get:Rule
-    val rule = InstantTaskExecutorRule()
-
     private val useCase: GetMovieUseCase = mockk()
     private val mapper: MovieToItemMovieMapper = mockk()
     private val mainViewModel: MainViewModel = MainViewModel(useCase, mapper)
 
-    private val testDispatcher = StandardTestDispatcher()
-
     @Before
     fun setUp() {
-        Dispatchers.setMain(testDispatcher)
+        Dispatchers.setMain(UnconfinedTestDispatcher())
     }
 
     @After
